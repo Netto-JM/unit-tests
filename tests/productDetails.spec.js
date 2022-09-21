d373a41ceb38de6de864d9dd1562e8b855c5d9de
NettoJM <nettojm86@gmail.com>
2022-09-21 20:51:26 -0300

    testing if productDetails returns an array

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -34,8 +34,9 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
   it('Teste se productDetails é uma função', () => {
     expect(productDetails).toBeInstanceOf(Function);
   });
-  // Teste se productDetails é uma função.
-  // Teste se o retorno da função é um array.
+  it('Teste se productDetails é uma função', () => {
+    expect(productDetails('Alcool gel', 'Máscara')).toBeInstanceOf(Array);
+  });
   // Teste se o array retornado pela função contém dois itens dentro.
   // Teste se os dois itens dentro do array retornado pela função são objetos.
   // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
